5abb153cc18790e936a6551a21f9388e20808ce8
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-08-08 23:24:01 +0530

    Added cancel for interactiveCpp subscription

--- lib/ffi/archiver_ffi/archiver_ffi.dart
+++ lib/ffi/archiver_ffi/archiver_ffi.dart
@@ -1,5 +1,7 @@
 import 'dart:ffi';
+import 'dart:io';
 import 'dart:isolate';
+import 'package:path/path.dart' as path;
 
 import 'package:squash_archiver/constants/app_files.dart';
 
@@ -10,28 +12,39 @@ class ArchiverFfi {
   DynamicLibrary _dylib;
 
   ArchiverFfi() {
-    _dylib = DynamicLibrary.open(AppFiles.ARCHIVER_FFI_LIB);
+    _dylib = DynamicLibrary.open(path.join(Directory.current.path, '../../../',
+        'native', 'archiver_lib', AppFiles.ARCHIVER_FFI_LIB));
+    //_dylib = DynamicLibrary.open(AppFiles.ARCHIVER_FFI_LIB); //todo
   }
 
   Future<void> run() async {
     final initializeApi = _dylib.lookupFunction<IntPtr Function(Pointer<Void>),
         int Function(Pointer<Void>)>('InitializeDartApi');
+
     if (initializeApi(NativeApi.initializeApiDLData) != 0) {
       throw 'Failed to initialize Dart API';
     }
 
-    final interactiveCppRequests = ReceivePort()
-      ..listen((data) {
-        print('Received: ${data} from Go');
-      });
+    final interactiveCppRequests = ReceivePort();
+
+    final interactiveCppSub = interactiveCppRequests.listen((data) {
+      print('Received: ${data} from Go');
+    });
+
     final nativePort = interactiveCppRequests.sendPort.nativePort;
 
     final startWork = _dylib
         .lookup<NativeFunction<StartWorkType>>('StartWork')
         .asFunction<StartWorkFunc>();
+
     startWork(nativePort);
 
-    while (true) {
+    await Future.delayed(const Duration(seconds: 10), () {
+      interactiveCppSub.cancel();
+      interactiveCppRequests.close();
+    });
+
+    for (var i = 0; i < 5;) {
       await Future.delayed(const Duration(seconds: 2));
       print('Dart: 2 seconds passed');
     }
