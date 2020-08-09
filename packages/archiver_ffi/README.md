28d8b2db9d9c9e6142da3a097796857ad978102d
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-08-09 13:05:48 +0530

    Added new bindings structure

--- packages/archiver_ffi/README.md
+++ packages/archiver_ffi/README.md
@@ -1,2 +1,22 @@
 # archiver_ffi
 
+Minimum requirement
+ - Dart 2.9.0
+
+MacOS:
+    Install Xcode.
+    Install LLVM -  
+    
+```shell
+brew install llvm.
+```
+
+To generate the bindings
+```shell
+  flutter pub run ffigen
+```
+
+To run
+```shell
+ dart ./lib/test_ffi.dart
+```
--- packages/archiver_ffi/lib/archiver_ffi.dart
+++ packages/archiver_ffi/lib/archiver_ffi.dart
@@ -2,25 +2,25 @@ import 'dart:ffi';
 import 'dart:io';
 import 'dart:isolate';
 import 'package:archiver_ffi/constants/app_files.dart';
-import 'package:archiver_ffi/generated_bindings.dart';
+import 'package:archiver_ffi/generated/bindings.dart';
 import 'package:path/path.dart' as path;
 
 typedef StartWorkType = Void Function(Int64 port);
 typedef StartWorkFunc = void Function(int port);
 
 class ArchiverFfi {
-  NativeLibrary _nativeLibrary;
+  SquashArchiverLib _squashArchiverLib;
 
   ArchiverFfi() {
     final _dylib = DynamicLibrary.open(path.join(Directory.current.path,
         'native', 'archiver_lib', AppFiles.ARCHIVER_FFI_LIB));
     //final _dylib = DynamicLibrary.open(AppFiles.ARCHIVER_FFI_LIB); //todo
 
-    _nativeLibrary = NativeLibrary(_dylib);
+    _squashArchiverLib = SquashArchiverLib(_dylib);
   }
 
   Future<void> run() async {
-    _nativeLibrary.InitializeDartApi(NativeApi.initializeApiDLData);
+    _squashArchiverLib.InitializeDartApi(NativeApi.initializeApiDLData);
 
     final interactiveCppRequests = ReceivePort();
 
@@ -30,7 +30,7 @@ class ArchiverFfi {
 
     final nativePort = interactiveCppRequests.sendPort.nativePort;
 
-    _nativeLibrary.StartWork(nativePort);
+    _squashArchiverLib.StartWork(nativePort);
 
     Future.delayed(const Duration(seconds: 10), () {
       interactiveCppSub.cancel();
