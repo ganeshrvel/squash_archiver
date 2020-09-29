717617e49501005080a97427321bb2a995cc2b2d
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-09-29 14:07:13 +0530

    Added Sparkle fw for auto update checking

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,7 +1,14 @@
 import Cocoa
 import FlutterMacOS
+import Sparkle
 
 class MainFlutterWindow: NSWindow {
+    @IBAction func checkForUpdates(_ sender: Any) {
+        let updater = SUUpdater.shared()
+        updater?.feedURL = URL(string: "https://sparkle-project.org/files/sparkletestcast.xml")
+        updater?.checkForUpdates(self)
+    }
+    
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
