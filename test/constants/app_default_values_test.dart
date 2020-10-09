51e16c947e3779d56efa7c698452066e25cc012f
Ganesh Rathinavel <ganeshrvel@outlook.com>
2020-10-09 13:28:17 +0530

    fixed SUPPORTED_ARCHIVE_EXTENSIONS test case

--- test/constants/app_default_values_test.dart
+++ test/constants/app_default_values_test.dart
@@ -1,4 +1,5 @@
 import 'package:archiver_ffi/archiver_ffi.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:squash_archiver/constants/app_default_values.dart';
 import 'package:squash_archiver/utils/utils/files.dart';
@@ -22,20 +23,19 @@ void main() {
 
     test('SUPPORTED_ARCHIVE_EXTENSIONS', () async {
       expect(
-        AppDefaultValues.SUPPORTED_ARCHIVE_EXTENSIONS,
-        containsAllInOrder([
-          'zip',
-          'tar',
-          'tar.br',
-          'tar.bz2',
-          'tar.gz',
-          'tar.lz4',
-          'tar.sz',
-          'tar.xz',
-          'tar.zst',
-          'rar',
-        ]),
-      );
+          listEquals(AppDefaultValues.SUPPORTED_ARCHIVE_EXTENSIONS, [
+            'zip',
+            'tar',
+            'tar.br',
+            'tar.bz2',
+            'tar.gz',
+            'tar.lz4',
+            'tar.sz',
+            'tar.xz',
+            'tar.zst',
+            'rar',
+          ]),
+          equals(true));
     });
   });
 }
